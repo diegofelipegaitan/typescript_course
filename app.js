32c92e849b6eb2000d77ed2d200542dbe44d1342
diegofelipegaitan <diegofelipegaitan81@gmail.com>
2018-03-08 05:54:15 -0500

    examples

--- app.js
+++ app.js
@@ -1,2 +1,61 @@
-var testVariable = "testing a text";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var testConstant = "testing a text";
+console.log(testConstant);
+var testVariable = "Test Variable";
+function reset() {
+    var testVariable = 20;
+    console.log(testVariable);
+}
+reset();
 console.log(testVariable);
+var sum = function (number1, number2) {
+    return number1 + number2;
+};
+console.log(sum(1, 2));
+var sumArrowFunction = function (number1, number2) { return number1 + number2; };
+console.log(sumArrowFunction(2, 1));
+var Adder = (function () {
+    function Adder(a) {
+        var _this = this;
+        this.a = a;
+        this.add = function (b) { return _this.a + b; };
+    }
+    return Adder;
+}());
+var ExtendedAdder = (function (_super) {
+    __extends(ExtendedAdder, _super);
+    function ExtendedAdder() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.superAdd = _this.add;
+        _this.add = function (b) {
+            return _this.superAdd(b);
+        };
+        return _this;
+    }
+    return ExtendedAdder;
+}(Adder));
+var extender = new ExtendedAdder(1);
+console.log("ExtendedAdder");
+console.log(extender.add(2));
+var countDown = function (counter) {
+    if (counter === void 0) { counter = 10; }
+    var originalCounter = counter;
+    while (counter > 0) {
+        counter--;
+    }
+    return "ORIGINAL COUNTER = " + originalCounter + " -- FINAL COUNTDOWN " + counter;
+};
+console.log(countDown(19));
+console.log(countDown());
+var numbers = [1, 65, 3, -2, 4];
+console.log(Math.max(1, 54, 32 - 4));
+console.log(Math.max.apply(Math, numbers));
--- app.ts
+++ app.ts
@@ -1,5 +1,56 @@
-const testVariable = "testing a text";
-console.log( testVariable );
+const testConstant = "testing a text";
+console.log(testConstant);
 
+let testVariable = "Test Variable";
 
+// block scope
+function reset() {
+    let testVariable = 20;
+    console.log(testVariable);
+}
+reset();
+console.log(testVariable);
 
+// arrow functions
+const sum = function (number1: number, number2: number): number {
+    return number1 + number2
+}
+console.log(sum(1, 2));
+
+const sumArrowFunction = (number1: number, number2: number) => number1 + number2;
+console.log(sumArrowFunction(2, 1));
+
+class Adder {
+    constructor(public a: number) { }
+    // This function is now safe to pass around
+    add = (b: number): number => this.a + b;
+}
+
+class ExtendedAdder extends Adder {
+    // Create a copy of parent before creating our own
+    private superAdd = this.add;
+    // Now create our override
+    add = (b: number): number => {
+        return this.superAdd(b);
+    }
+}
+
+let extender = new ExtendedAdder(1);
+console.log("ExtendedAdder");
+console.log(extender.add(2));
+
+// Functions and Defaults Values
+const countDown = (counter: number = 10): string => {
+    let originalCounter = counter;
+    while (counter > 0) {
+        counter--;
+    }
+    return "ORIGINAL COUNTER = " + originalCounter + " -- FINAL COUNTDOWN " + counter;
+}
+console.log(countDown(19));
+console.log(countDown());
+
+// Rest and Spread
+let numbers = [1, 65, 3, -2, 4];
+console.log(Math.max(1, 54, 32 - 4));
+console.log(Math.max(...numbers));
