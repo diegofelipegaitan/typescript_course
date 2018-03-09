f06caf953df832bf6e2bf25c85d2bfc1147521f5
diegofelipegaitan <diegofelipegaitan81@gmail.com>
2018-03-09 14:54:32 -0500

    methods, setters and getters

--- app.js
+++ app.js
@@ -2,8 +2,33 @@ var Person = (function () {
     function Person(name, username) {
         this.name = name;
         this.username = username;
+        this.age = 26;
     }
+    Object.defineProperty(Person.prototype, "$typePerson", {
+        get: function () {
+            return this.typePerson;
+        },
+        set: function (value) {
+            this.typePerson = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Person.prototype, "$age", {
+        get: function () {
+            return this.age;
+        },
+        set: function (value) {
+            this.age = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Person.prototype.printAge = function () {
+        console.log(this.age);
+    };
     return Person;
 }());
 var person = new Person("Diego", "proimond");
+person.$typePerson = "Diego";
 console.log(person);
--- app.ts
+++ app.ts
@@ -1,11 +1,32 @@
 class Person {
 
-    //private typePerson: string;
-    protected age: number;
+    protected typePerson: string;
+    private age: number = 26;
 
-    constructor(public name: string, public username: string) { }
+    public get $typePerson(): string {
+        return this.typePerson;
+    }
+
+    public set $typePerson(value: string) {
+        this.typePerson = value;
+    }
+
+    public get $age(): number {
+        return this.age;
+    }
+
+    public set $age(value: number) {
+        this.age = value;
+    }
+
+    constructor(public name: string, public username: string) {
+    }
+
+    printAge() {
+        console.log(this.age);
+    }
 }
 
 const person = new Person("Diego", "proimond");
-
+person.$typePerson = "Diego";
 console.log(person);
