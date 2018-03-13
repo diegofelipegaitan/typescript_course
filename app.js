37a9bb49c799a56eca36d2a9951df6b5d721ace6
diegofelipegaitan <diegofelipegaitan81@gmail.com>
2018-03-12 22:10:56 -0500

    definition

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-"use strict";
+function Car(name) {
+    this.name = name;
+    this.acceleration = 0;
+    this.honk = function () {
+        console.log("Toooooooooot!");
+    };
+    this.accelerate = function (speed) {
+        this.acceleration = this.acceleration + speed;
+    };
+}
+var car = new Car("BMW");
+car.honk();
+console.log(car.acceleration);
+car.accelerate(10);
+console.log(car.acceleration);
