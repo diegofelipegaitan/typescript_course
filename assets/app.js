47194110dd7ab55302dd690958811e11c53de0d1
diegofelipegaitan <diegofelipegaitan81@gmail.com>
2018-03-23 10:28:54 -0500

    Importing rectangle

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var circle_1 = require("./math/circle");
+var rectangle_1 = require("./math/rectangle");
 var circle = circle_1.Circle.getInstance();
+var rectangle = rectangle_1.Rectangle.getInstance();
 console.log(circle.calculateCirc(2));
+console.log(rectangle.calculateRectangule(1, 2));
--- resources/app.ts
+++ resources/app.ts
@@ -1,4 +1,8 @@
 import {Circle} from './math/circle';
+import { Rectangle } from './math/rectangle';
 
 let circle = Circle.getInstance();
+let rectangle = Rectangle.getInstance();
+
 console.log( circle.calculateCirc(2) );
+console.log( rectangle.calculateRectangule( 1 , 2 ) );
