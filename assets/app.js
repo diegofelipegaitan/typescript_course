f31fa1ad559c77ade871a4f27baf57652884625c
Diego Gaitán <felipe.gaitan.81@gmail.com>
2018-04-18 14:45:46 -0500

    function inside the interface

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,19 @@
 "use strict";
 function greet(person) {
     console.log("Hello " + person.name);
+    console.log(person.propName);
 }
 function changeName(person) {
     person.name = "Diego Felipe";
 }
 var person = {
     name: "Diego",
-    age: 36
+    hobbies: ['read', 'tv', 'bicycle'],
+    greet: function (lastName) {
+        console.log("Hello " + lastName);
+    }
 };
 greet(person);
 changeName(person);
 greet(person);
+person.greet("ME");
--- resources/app.ts
+++ resources/app.ts
@@ -1,19 +1,29 @@
 interface NamePerson {
-    name: string
+    name: string;
+    age?: number;
+    [propName: string]: any;
+
+    greet(lastName: string): void;
 }
 
 function greet( person: NamePerson){
     console.log("Hello " + person.name );
+    console.log( person.propName );
 }
 
 function changeName( person: NamePerson){
     person.name = "Diego Felipe";
 }
 
-const person = {
+const person: NamePerson = {
     name: "Diego",
-    age : 36
+    hobbies: ['read','tv','bicycle'],
+
+    greet( lastName: string ){
+        console.log( "Hello "+ lastName );
+    }
 }
 greet( person );
 changeName( person );
 greet( person );
+person.greet( "ME" );
